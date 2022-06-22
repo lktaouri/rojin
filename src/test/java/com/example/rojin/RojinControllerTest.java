2229b4ff1b5798d7995ebc89390e07af40a09c0f
lktaouri <mohammedlaktaoui@outlook.it>
2022-06-22 12:19:40 +0200

    Hello Rojin

--- src/test/java/com/example/rojin/RojinControllerTest.java
+++ src/test/java/com/example/rojin/RojinControllerTest.java
@@ -1,7 +1,17 @@
 package com.example.rojin;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RojinControllerTest {
+    RojinController testen = new RojinController();
+
+    @Test
+    void testingYourMOM(){
+        String a = testen.index();
+        String b = "Rojin";
+        assertEquals(a,b);
+    }
 
 }
