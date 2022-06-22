7cde2373eb3b3706e0a54c4b21ac76f9671b3cad
lktaouri <mohammedlaktaoui@outlook.it>
2022-06-22 11:24:51 +0200

    Initial commit

---
+++ src/main/java/com/example/rojin/RojinApplication.java
@@ -0,0 +1,13 @@
+package com.example.rojin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RojinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RojinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rojin/RojinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rojin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RojinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
